608b4618d342b79d8e5b7e080bf70d9953fa09a4
devvMuhammad <muhammadaljoufi@gmail.com>
2024-06-14 01:37:22 +0500

    fix: corrected path in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <div className="flex min-h-screen flex-col space-y-6">
       Yo this is the main page <br />
-      For dashboard, click <Link href="/dashboard">Here</Link>
+      For dashboard, click <Link href="/admin/dashboard">Here</Link>
     </div>
   );
 }
