6de30e9d0216af5e3faccc91cb3a6eee2517acf8
devvMuhammad <muhammadaljoufi@gmail.com>
2024-06-14 01:38:47 +0500

    fix: added key prop

--- src/components/editor/subject-questions.tsx
+++ src/components/editor/subject-questions.tsx
@@ -29,7 +29,11 @@ export default function SubjectQuestions({ mcqs }: { mcqs: SubjectType[] }) {
       <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
         {/* //!Subject MCQs Start Here */}
         {subject.questions.map((question, index) => (
-          <QuestionCard number={index + 1} question={question} />
+          <QuestionCard
+            key={question.question}
+            number={index + 1}
+            question={question}
+          />
         ))}
       </div>
     </div>
