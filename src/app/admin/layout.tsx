93f412dcdb25bf3cd7f0c237b93c7e5ddf5187d8
devvMuhammad <muhammadaljoufi@gmail.com>
2024-06-15 01:54:22 +0500

    style: added blur effect on elements beneath nav

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -5,7 +5,7 @@ import { dashboardConfig } from "@/config/dashboard";
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col space-y-6">
-      <header className="sticky top-0 z-40 border-b bg-background">
+      <header className="sticky top-0 z-40 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 ">
         <div className="container flex h-16 items-center justify-between py-2">
           <MainNav items={dashboardConfig.mainNav} />
           <UserProfile
