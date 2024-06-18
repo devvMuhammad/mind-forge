59630180b99f6f325bf1fabe93d1e8c041163d09
devvMuhammad <muhammadaljoufi@gmail.com>
2024-06-18 19:56:26 +0500

    feat: refactored the create test functionality in a separate form component

--- src/components/create-test.tsx
+++ src/components/create-test.tsx
@@ -1,38 +1,21 @@
 "use client";
-import { createTest } from "@/app/actions/create-test";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { testsConfig } from "@/config/tests";
-import { toTitleCase } from "@/lib/utils";
-// import { useAction } from "next-safe-action/hooks";
+
+import CreateTestForm from "./create-test-form";
 
 export default function CreateTestButton() {
-  // const { execute } = useAction(createTest, { onSuccess: (args) => args.data });
-
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button>Create Test</Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>Select Category</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          {testsConfig.categories.map((category) => (
-            <DropdownMenuItem key={category}>
-              {toTitleCase(category)}
-            </DropdownMenuItem>
-          ))}
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
+      <DropdownMenuContent className="p-4">
+        <CreateTestForm />
       </DropdownMenuContent>
     </DropdownMenu>
   );
