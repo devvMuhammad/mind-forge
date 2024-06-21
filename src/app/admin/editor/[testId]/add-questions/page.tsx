1319cfbb6269102d66cb19b7b15977c8d408d958
devvMuhammad <muhammadaljoufi@gmail.com>
2024-06-21 10:42:11 +0500

    ts: some ts issues

--- src/app/admin/editor/[testId]/add-questions/page.tsx
+++ src/app/admin/editor/[testId]/add-questions/page.tsx
@@ -1,45 +1,48 @@
 import { toTitleCase } from "@/lib/utils";
 import EditingArea from "@/components/add-questions/editing-area";
+import { testsConfig } from "@/config/tests";
+import { redirect } from "next/navigation";
 
 export default function page({
   params: { testId },
+  searchParams: { subject },
 }: {
   params: { testId: string };
+  searchParams: { subject: string };
 }) {
+  // console.log(searchParams);
+  if (!subject || testsConfig.subjects.indexOf(subject as any) === -1) {
+    redirect(`/admin/dashboard/${testId}`);
+  }
   // !fetch the title properties based on the testID and then pass it onto the questions title
-  const dummyFetchedData = {
-    title: "Demo Test for Engineering Before NET-4",
-    category: "pre-engineering",
-    subject: "Mathematics",
-  };
-  const { title, category, subject } = dummyFetchedData;
+  //? or a better way would be to fetch the data using context
   return (
     <section>
-      <AddQuestionsTitle title={title} category={category} subject={subject} />
-      <EditingArea />
+      <AddQuestionsTitle subject={subject} />
+      <EditingArea testId={testId} currentSubject={subject} />
     </section>
   );
 }
 
 type AddQuestionsTitleProps = {
-  title: string;
-  category: string;
+  // title: string;
+  // category: string;
   subject: string;
 };
 function AddQuestionsTitle({
-  title,
-  category,
+  // title,
+  // category,
   subject,
 }: AddQuestionsTitleProps) {
   return (
     <>
       <h1 className="text-2xl font-bold">Interface for Adding MCQs</h1>
-      <h3 className=" font-bold">
+      {/* <h3 className=" font-bold">
         Title: <span className="font-normal">{title}</span>{" "}
-      </h3>
-      <h3 className=" font-bold">
+      </h3> */}
+      {/* <h3 className=" font-bold">
         Category: <span className="font-normal">{toTitleCase(category)}</span>{" "}
-      </h3>
+      </h3> */}
       <h3 className=" font-bold">
         Subject: <span className="font-normal">{toTitleCase(subject)}</span>{" "}
       </h3>
--- src/app/admin/editor/[testId]/page.tsx
+++ src/app/admin/editor/[testId]/page.tsx
@@ -1,6 +1,8 @@
 import SubjectQuestions from "@/components/editor/subject-questions";
 import TestDetails from "@/components/editor/test-details";
 import { TestType } from "@/types";
+import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
 
 type TestEditorPageProps = {
   params: {
@@ -13,10 +15,25 @@ type TestEditorPageData = {
   mcqsLength: number;
 };
 
-export default function TestEditorPage({
+export default async function TestEditorPage({
   params: { testId },
 }: TestEditorPageProps) {
-  const dummyData: TestEditorPageData = {
+  // prisma.questions.grou
+  const test1 = await prisma.tests.findUnique({
+    where: { id: testId },
+    include: { questions: true },
+  });
+  // const test2 = await prisma.questions.groupBy({
+  //   where: {
+  //     testId: {
+  //       equals: testId,
+  //     },
+  //   },
+  //   by: "subject",
+  // });
+  // console.log("details of test2", test2);
+  if (!test1) redirect("/admin/dashboard");
+  const dummyData = {
     testData: {
       testId,
       title: "Test Title",
@@ -28,12 +45,22 @@ export default function TestEditorPage({
             {
               statement:
                 "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem inventore id dolore tempore repellendus voluptatibus sint dolorum velit quaerat nihil cum harum nemo ad quam, neque ullam asperiores incidunt impedit corrupti ab mollitia ut? Facilis cum laboriosam molestiae dolor praesentium eveniet fuga perspiciatis, culpa debitis minima in alias nobis quae",
-              options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+              options: ["Option 1", "Option 2", "Option 3", "Option 4"] as [
+                string,
+                string,
+                string,
+                string
+              ],
               answer: 0,
             },
             {
               statement: "Question 2",
-              options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+              options: ["Option 1", "Option 2", "Option 3", "Option 4"] as [
+                string,
+                string,
+                string,
+                string
+              ],
               answer: 1,
             },
           ],
@@ -62,12 +89,13 @@ export default function TestEditorPage({
   return (
     <section>
       <TestDetails
-        category={dummyData.testData.category}
-        mcqsLength={dummyData.mcqsLength}
-        lastChangedBy={dummyData.testData.lastChangedBy}
-        lastChangedDate={dummyData.testData.lastChangedDate}
+        category={test1.category}
+        mcqsLength={test1.questions.length}
+        lastChangedBy={test1.lastChangedBy}
+        lastChangedDate={test1.lastChanged}
       />
       <SubjectQuestions testId={testId} mcqs={dummyData.testData.mcqs} />
+      {/* <SubjectQuestions testId={testId} /> */}
     </section>
   );
 }
