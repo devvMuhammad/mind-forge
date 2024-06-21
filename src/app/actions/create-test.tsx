9d2dee88449253833008ccc65ce2dab8b6be8293
devvMuhammad <muhammadaljoufi@gmail.com>
2024-06-21 10:39:11 +0500

    feat: create test server action

--- src/app/actions/create-test.tsx
+++ src/app/actions/create-test.tsx
@@ -1,16 +1,32 @@
 "use server";
 
-import { TestType } from "@/types";
+import { PossibleCategoryType, QuestionType, TestType } from "@/types";
 import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+import { Prisma, QuestionSubject } from "@prisma/client";
+import { DefaultArgs } from "@prisma/client/runtime/library";
 
-export async function createTest({ category, title, lastChangedBy }: TestType) {
-  // return await prisma.tests.create({
-  //   data: {
-  //     category: category as any,
-  //     title,
-  //     lastChangedBy,
-  //     lastChanged: new Date(),
-  //     // atte
-  //   },
-  // });
+type ActionProps = {
+  category: PossibleCategoryType;
+  title: string;
+  lastChangedBy: string;
+};
+// type a = Prisma.TestsCreateArgs<DefaultArgs>;
+
+export async function createTest({
+  category,
+  title,
+  lastChangedBy,
+}: ActionProps) {
+  // try {
+  const data = await prisma.tests.create({
+    data: {
+      category,
+      title,
+      lastChangedBy,
+      attempts: 0,
+    },
+  });
+  revalidatePath("/admin/dashboard");
+  return { success: true, data };
 }
