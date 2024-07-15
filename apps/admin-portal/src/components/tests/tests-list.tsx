04f1cae30c034c5cc3ec6a4454077f0acbc76524
devvMuhammad <muhammadaljoufi@gmail.com>
2024-07-15 21:05:58 +0500

    feat: converted main page to supabase orm

--- apps/admin-portal/src/components/tests/tests-list.tsx
+++ apps/admin-portal/src/components/tests/tests-list.tsx
@@ -1,19 +1,18 @@
 import TestCard from "@/components/tests/test-card";
-import { prisma } from "@/lib/prisma";
-
-async function getTests() {
-  const data = await prisma.tests.findMany({
-    include: {
-      questions: true,
-    },
-  });
-  console.log(data);
-  return data;
-}
+import { createClient } from "@/lib/supabase/server";
 
 export default async function TestsList() {
-  const data = await getTests();
-  return data.length > 0 ? (
+  const supabase = createClient();
+  const data = (await supabase.from("tests").select("*, questions(*)")).data;
+
+  if (!data || data.length === 0)
+    return (
+      <h1 className="text-xl">
+        No Snippets found at the moment. Start adding some by creating some :)
+      </h1>
+    );
+
+  return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
       {data.map((test) => (
         <TestCard
@@ -23,14 +22,10 @@ export default async function TestsList() {
           category={test.category}
           questionsLength={test.questions.length}
           attempts={test.attempts}
-          createdAt={test.createdAt}
-          lastChanged={test.lastChanged}
+          createdAt={new Date(test.created_at)}
+          lastChanged={new Date(test.last_changed)}
         />
       ))}
     </div>
-  ) : (
-    <h1 className="text-xl">
-      No Snippets found at the moment. Start adding some by creating some :)
-    </h1>
   );
 }
