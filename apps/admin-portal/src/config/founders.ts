c13a8a9a2663ee8159d9cef3273d01d2eb3d0550
devvMuhammad <muhammadaljoufi@gmail.com>
2024-07-15 21:05:43 +0500

    feat: changed structure of founders array

--- apps/admin-portal/src/config/founders.ts
+++ apps/admin-portal/src/config/founders.ts
@@ -1,6 +1,6 @@
 export const founders = [
-  "CTO Muhammad",
-  "CEO Muhammad",
-  "COO Umar",
-  "Senior Intern Asjad",
-] as const;
+  { title: "CTO Muhammad", name: "muhammad" },
+  { title: "CEO Muhammad", name: "muhammadijaz" },
+  { title: "COO Umar", name: "umar" },
+  { title: "Senior Intern Asjad", name: "asjad" },
+] as const satisfies { title: string; name: string }[];
