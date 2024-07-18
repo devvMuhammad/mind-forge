9f6bfe40f695824f7b8f5e64c56cd5fad335acdf
devvMuhammad <muhammadaljoufi@gmail.com>
2024-07-18 12:21:46 +0500

    feat: added categories config array and category type

---
+++ apps/student-portal/src/config/categories.ts
@@ -0,0 +1,8 @@
+import { CategoryType } from "@/types";
+
+export const categories: { name: CategoryType; title: string }[] = [
+  { name: "business", title: "Business" },
+  { name: "engineering", title: "Engineering" },
+  { name: "ics", title: "ICS" },
+  { name: "medical", title: "Pre Medical" },
+];
---
+++ apps/student-portal/src/types/index.d.ts
@@ -0,0 +1,3 @@
+import { Database } from "./supabase";
+
+export type CategoryType = Database["public"]["Enums"]["TestCategory"];
